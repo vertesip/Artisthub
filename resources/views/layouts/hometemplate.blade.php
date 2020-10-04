9b91ed8a2d75db228926f8024d8ad0b5c4801516
vertesip <vertesip@gain.uni-sopron.hu>
2020-10-04 20:54:12 +0200

    -

--- resources/views/layouts/hometemplate.blade.php
+++ resources/views/layouts/hometemplate.blade.php
@@ -63,11 +63,11 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('home') }}"
+                                <a class="dropdown-item" href="{{ route('home') }}">
                                 <button type="button" onclick="window.location='{{ url("home") }}'"></button>
                                 {{ __('Home') }}
                                 </a>
-                               <a class="dropdown-item" href="{{'profile/(Auth::user()->id)' }}"
+                               <a class="dropdown-item" href="{{'profile/(Auth::user()->id)' }}">
                                 <button type="button" onclick="window.location='{{ url("profile") }}'"></button>
                                     {{ __('Profile') }}
                                 </a>
