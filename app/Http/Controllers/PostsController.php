153d60602f27339ec07b2a518a4c6f3b683e510b
vertesip <vertesip@gain.uni-sopron.hu>
2020-10-17 19:43:58 +0200

    - Posztoláshoz megcsináltam a a táblát (ahova letárolom) Controllert, a post formot a create.blade.php-ban, és megadtam a routeokat a web.php-ban, egyenlőre a képekre, az audiot holnap tesztelem.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,4 +7,16 @@ class PostsController extends Controller
     public function create(){
         return view('posts.create');
     }
+
+
+    public function store()
+    {
+        $data = request()->validate([
+            'text' => 'required',
+            'image' => ['required', 'image'],
+        ]);
+
+        dd(request()->all());
+    }
+
 }
